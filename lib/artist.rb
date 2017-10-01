cdf95fcabe912c11e31c547e1a2f3f8909dd4e26
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-01 15:22:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,17 @@
 class Artist
   attr_accessor :name
 
-  def initialize 
+  def initialize
     @name = name
     @songs = []
   end
 
+def add_song(song)
+  @songs << song
+  song.artist = self
+end
+
+def songs
+  @songs
+end
 end
